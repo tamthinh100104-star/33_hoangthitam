5e8798b224c980fa8f3b505f18aa9974849828a5
tamthinh100104-star <tamthinh100104@gmail.com>
2025-12-07 22:24:28 +0700

    feat: implement add student

--- 33_hoangthitam.py
+++ 33_hoangthitam.py
@@ -12,7 +12,16 @@ def add_student(name, year_of_birth, address):
     - In ra thông báo "Da them sinh vien <ten> thanh cong."
     """
     ### VIẾT CODE CỦA BẠN VÀO ĐÂY ###
-    pass  # Xóa dòng này khi bạn viết code
+  def add_student(name, year_of_birth, address):
+    student = {
+        "name": name,
+        "year_of_birth": year_of_birth,
+        "address": address
+    }
+    student_list.append(student)
+    print(f"Da them sinh vien {name} thanh cong.")
+
+
 
 def print_student_list():
     """
